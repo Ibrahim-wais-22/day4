a5059479ad90800dd3ca378efd9e2713fc3ab847
ibrahim wais <67577024+Ibrahim-wais-22@users.noreply.github.com>
2022-09-19 05:50:47 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,14 @@ const array = ['Orange','Apple','Pineapple','Grapes'];
 // map -- fliter -- find
 
 const mappArray = array.map((item) =>{
-    console.log(item);
-    document.querySelector('#map').innerHTML = item;
-} );
-// array.forEach(element => {
-//     document.querySelector('#map').innerHTML = element;
-//     console.log(element)
-// });
+        console.log(item);
+        document.querySelector('#map').innerHTML = item;
+    });
 
-const filterArray = array.filter(item => typeof item === 'string');
+const filterArray = array.filter(word => word=="Orange" || word=="Grapes" );
+console.log(filterArray)
+
+
 document.querySelector('#filter').innerHTML = filterArray;
 
 const findArray = array.find(item => item === "Apple")
@@ -20,4 +19,3 @@ let time = new Date();
 document.querySelector('#time').innerHTML = time.getHours();
 let time1 = new Date();
 document.querySelector('#time1').innerHTML = time;
-// عمل غير مكتمل
